b887f0abd48dc7f0f25fbf5f08d78a38da62b032
phatdroid94 <phatdroid94@gmail.com>
2020-10-17 20:41:37 +0700

    layout random

--- app/src/main/java/com/example/apppimagepicker25082020/HomeActivity.java
+++ app/src/main/java/com/example/apppimagepicker25082020/HomeActivity.java
@@ -1,13 +1,27 @@
 package com.example.apppimagepicker25082020;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.apppimagepicker25082020.databinding.ActivityHomeBinding;
 
 public class HomeActivity extends AppCompatActivity {
 
+    ActivityHomeBinding mBinding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        mBinding = ActivityHomeBinding.inflate(getLayoutInflater());
+        setContentView(mBinding.getRoot());
+
+        mBinding.buttonNewGame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeActivity.this , RandomActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/apppimagepicker25082020/ListImageActivity.java
@@ -0,0 +1,18 @@
+package com.example.apppimagepicker25082020;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.apppimagepicker25082020.databinding.ActivityHomeBinding;
+
+public class ListImageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_image);
+
+    }
+}
