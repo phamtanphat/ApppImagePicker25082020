75dd47163db4c043a2a7232ffc6fd862489ea1ef
phatdroid94 <phatdroid94@gmail.com>
2020-10-20 20:54:08 +0700

    get array images

--- app/src/main/java/com/example/apppimagepicker25082020/ListImageActivity.java
+++ app/src/main/java/com/example/apppimagepicker25082020/ListImageActivity.java
@@ -1,18 +1,40 @@
 package com.example.apppimagepicker25082020;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TableRow;
 
 import com.example.apppimagepicker25082020.databinding.ActivityHomeBinding;
+import com.example.apppimagepicker25082020.databinding.ActivityListImageBinding;
+
+import java.util.Random;
 
 public class ListImageActivity extends AppCompatActivity {
 
+    ActivityListImageBinding mBinding;
+    String[] mArrayNameImages;
+    int mColumn = 3;
+    int mRow = 6;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_list_image);
+        mBinding = ActivityListImageBinding.inflate(getLayoutInflater());
+        setContentView(mBinding.getRoot());
 
+        mArrayNameImages = getResources().getStringArray(R.array.array_name_images);
+
+        for (int i = 1 ; i <= mRow ; i++){
+            TableRow tableRow = new TableRow(this);
+            for (int y = 1 ; y <= mColumn ; y++){
+                ImageView imageView = new ImageView(this);
+                imageView.setImageResource(R.drawable.bo);
+                tableRow.addView(imageView);
+            }
+            mBinding.tableLayout.addView(tableRow);
+        }
     }
 }
