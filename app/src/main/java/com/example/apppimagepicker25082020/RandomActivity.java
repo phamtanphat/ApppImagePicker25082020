f592c59675a5da609def86b55d26103de5196cd9
phatdroid94 <phatdroid94@gmail.com>
2020-10-20 20:00:40 +0700

    update time

--- app/src/main/java/com/example/apppimagepicker25082020/RandomActivity.java
+++ app/src/main/java/com/example/apppimagepicker25082020/RandomActivity.java
@@ -16,13 +16,37 @@ public class RandomActivity extends AppCompatActivity {
     String[] mArrayNameImages;
     int mIdImageRandom = -1;
     int mIndex = -1;
-
+    int mScore = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mBinding = ActivityRandomBinding.inflate(getLayoutInflater());
         setContentView(mBinding.getRoot());
+        initView();
+        randomImage();
+        countDownTime();
+    }
 
+    private void initView() {
+        mBinding.tvScore.setText("Score : " + mScore);
+    }
+
+    private void countDownTime() {
+        MyCountDown.getInstance().startTimeOut(4200 , 1000);
+        MyCountDown.getInstance().setOnListenerCountDown(new OnListenerCountDown() {
+            @Override
+            public void onTick(long countDownInterval) {
+                mBinding.tvTime.setText("Time : " + (countDownInterval / 1000));
+            }
+
+            @Override
+            public void onFinish() {
+
+            }
+        });
+    }
+
+    private void randomImage() {
         mArrayNameImages = getResources().getStringArray(R.array.array_name_images);
         mIndex = new Random().nextInt(mArrayNameImages.length);
         mIdImageRandom = getResources().getIdentifier(
